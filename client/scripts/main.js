341aac56d8aeddd6cec367e81e3aaa334e2bab9c
David Blood <davidbjourno@users.noreply.github.com>
2016-07-11 21:56:03 +0100

    Fix smooth scroll issue with accented characters; add #top anchor

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -420,7 +420,7 @@ $(document).ready(function(){
       event.preventDefault();
 
       // Store hash
-      var hash = this.hash;
+      var hash = decodeURIComponent(this.hash);
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
