86f8826cf8b25537d5ac18e39ff84414ed019da2
David Blood <davidbjourno@users.noreply.github.com>
2016-07-11 22:10:34 +0100

    Fine-tune smooth scrolling

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -412,7 +412,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $('a').on('click', function(event) {
+  $('a.ceo-anchor, a.top-link').on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
