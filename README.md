1afa0b481409e9e022529c98833330f9ee644e33
David Blood <davidbjourno@users.noreply.github.com>
2017-06-13 11:49:13 +0100

    Re-scaffold using starter kit commit c1069a6

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
-# bank-ceos-pay
+# [bank-ceos-pay](https://ig.ft.com/sites/bank-ceo-compensation/2017)
 
-Update to the annual <a href="https://ig.ft.com/sites/bank-ceo-compensation/2016/">FT/Equilar bank CEO pay survey</a>
+> 
+
+[![Build Status][circle-image]][circle-url] [![Dependency Status][devdeps-image]][devdeps-url]
+
+## Local
+
+```
+npm start
+```
+
+Build/compile, start a dev server and watches for changes.
+
+# Deploy
+
+1. Write code in a branch.
+2. Make a PR. CI will automatically:
+    * build and test the branch
+    * deploy green builds to the review site
+3. Do quick smoke testing of the review build
+4. Get a code review. Once you get a thumbs up, merge into master.
+5. CI will build, test and deploy a build to Production.
+
+
+## Uses Starter Kit
+
+This project was scaffolded with [Starter Kit @c1069a6](https://github.com/ft-interactive/starter-kit/tree/c1069a6).
 
 ## Licence
-This software is published by the Financial Times under the [MIT licence](http://opensource.org/licenses/MIT). 
+This software is published by the Financial Times under the [MIT licence](http://opensource.org/licenses/MIT).
 
 Please note the MIT licence includes only the software, and does not cover any FT content made available using the software, which is copyright &copy; The Financial Times Limited, all rights reserved. For more information about re-publishing FT content, please contact our [syndication department](http://syndication.ft.com/).
+
+<!-- badge URLs -->
+[circle-url]: https://circleci.com/gh/ft-interactive/bank-ceos-pay
+[circle-image]: https://circleci.com/gh/ft-interactive/bank-ceos-pay/tree/master.svg?style=shield
+
+[devdeps-url]: https://david-dm.org/ft-interactive/bank-ceos-pay#info=devDependencies
+[devdeps-image]: https://img.shields.io/david/dev/ft-interactive/bank-ceos-pay.svg?style=flat-square
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,26 @@
-/* eslint-disable no-console, global-require */
+/* eslint-disable no-console, global-require, import/no-extraneous-dependencies */
 
 import browserify from 'browserify';
 import browserSync from 'browser-sync';
-import del from 'del';
 import gulp from 'gulp';
-import igdeploy from 'igdeploy';
 import mergeStream from 'merge-stream';
 import path from 'path';
 import runSequence from 'run-sequence';
 import source from 'vinyl-source-stream';
-import subdir from 'subdir';
-import vinylBuffer from 'vinyl-buffer';
 import watchify from 'watchify';
 import AnsiToHTML from 'ansi-to-html';
+import gulpnunjucks from 'gulp-nunjucks';
+import inlineSource from 'gulp-inline-source';
+import htmlmin from 'gulp-htmlmin';
+import rev from 'gulp-rev';
+import revReplace from 'gulp-rev-replace';
+import gulpdata from 'gulp-data';
+import sass from 'gulp-sass';
+import util from 'gulp-util';
+import autoprefixer from 'gulp-autoprefixer';
+import plumber from 'gulp-plumber';
+import http from 'http';
 
-const $ = require('auto-plug')('gulp');
 const ansiToHTML = new AnsiToHTML();
 
 const AUTOPREFIXER_BROWSERS = [
@@ -25,24 +31,29 @@ const AUTOPREFIXER_BROWSERS = [
   'Safari >= 7',
 ];
 
-const DEPLOY_TARGET = ''; // e.g. 'features/YOUR-PROJECT-NAME'
-
 const BROWSERIFY_ENTRIES = [
-  'scripts/main.js',
+  'index.js',
 ];
 
 const BROWSERIFY_TRANSFORMS = [
   'babelify',
   'debowerify',
-  'hbsfy',
 ];
 
 const OTHER_SCRIPTS = [
-  'scripts/top.js',
+  'components/core/top.js',
 ];
 
-let env = 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+const copyGlob = OTHER_SCRIPTS.concat([
+  'client/**/*',
+  '!client/**/*.{html,scss}',
+
+  // REPLACE: if using imagmin
+  // '!client/**/*.{jpg,png,gif,svg}',
+
+]);
 
 // helpers
 let preventNextReload; // hack to keep a BS error notification on the screen
@@ -56,9 +67,9 @@ function reload() {
 }
 
 function handleBuildError(headline, error) {
-  if (env === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     // show in the terminal
-    $.util.log(headline, error && error.stack);
+    util.log(headline, error && error.stack);
 
     // report it in browser sync
     let report = (
@@ -81,31 +92,36 @@ function handleBuildError(headline, error) {
 
 // function to get an array of objects that handle browserifying
 function getBundlers(useWatchify) {
-  return BROWSERIFY_ENTRIES.map(entry => {
+  return BROWSERIFY_ENTRIES.map((entry) => {
     const bundler = {
       b: browserify(path.posix.resolve('client', entry), {
         cache: {},
         packageCache: {},
         fullPaths: useWatchify,
         debug: useWatchify,
+        standalone: 'client',
       }),
 
       execute() {
-        let stream = this.b.bundle()
-          .on('error', error => {
+        const stream = this.b.bundle()
+          .on('error', function browserifyError(error) {
             handleBuildError.call(this, 'Error building JavaScript', error);
           })
-          .pipe(source(entry.replace(/\.js$/, '.bundle.js')));
+          .pipe(source(entry));
 
+        // If you want JS sourcemaps:
+        //    1. npm i -D gulp-sourcemaps
+        //    2. uncomment code below
+        //
         // skip sourcemap creation if we're in 'serve' mode
-        if (useWatchify) {
-          stream = stream
-            .pipe(vinylBuffer())
-            .pipe($.sourcemaps.init({ loadMaps: true }))
-            .pipe($.sourcemaps.write('./'));
-        }
+        // if (useWatchify) {
+        //   stream = stream
+        //    .pipe(vinylBuffer())
+        //    .pipe(gulpsourcemaps.init({ loadMaps: true }))
+        //    .pipe(gulpsourcemaps.write('./'));
+        // }
 
-        return stream.pipe(gulp.dest('.tmp'));
+        return stream.pipe(gulp.dest('dist'));
       },
     };
 
@@ -115,14 +131,9 @@ function getBundlers(useWatchify) {
     // upgrade to watchify if we're in 'serve' mode
     if (useWatchify) {
       bundler.b = watchify(bundler.b);
-      bundler.b.on('update', files => {
+      bundler.b.on('update', () => {
         // re-run the bundler then reload the browser
         bundler.execute().on('end', reload);
-
-        // also report any linting errors in the changed file(s)
-        gulp.src(files.filter(file => subdir(path.resolve('client'), file)))
-          .pipe($.eslint())
-          .pipe($.eslint.format());
       });
     }
 
@@ -130,56 +141,23 @@ function getBundlers(useWatchify) {
   });
 }
 
-// compresses images (client => dist)
-gulp.task('images', () => gulp.src('client/**/*.{jpg,png,gif,svg}')
-  .pipe($.imagemin({
-    progressive: true,
-    interlaced: true,
-  }))
-  .pipe(gulp.dest('dist'))
-);
+/**
+ * The Tasks
+ */
 
-// copies over miscellaneous files (client => dist)
-gulp.task('copy', () => gulp.src(
-  OTHER_SCRIPTS.concat([
-    'client/**/*',
-    '!client/**/*.{html,scss,js,jpg,png,gif,svg}', // all handled by other tasks
-  ]), { dot: true })
-  .pipe(gulp.dest('dist'))
-);
-
-gulp.task('build-pages', () =>
-  gulp.src(['client/**/*.html', '!client/includes/**.html'])
-      .pipe($.htmlTagInclude()).pipe(gulp.dest('.tmp'))
-);
-
-// minifies all HTML, CSS and JS (.tmp & client => dist)
-gulp.task('html', done => {
-  const assets = $.useref.assets({
-    searchPath: ['.tmp', 'client', '.'],
-  });
-
-  gulp.src('.tmp/**/*.html')
-    .pipe(assets)
-    .pipe($.if('*.js', $.uglify({ output: { inline_script: true } })))
-    .pipe($.if('*.css', $.minifyCss({ compatibility: '*' })))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe(gulp.dest('dist'))
-    .on('end', () => {
-      gulp.src('dist/**/*.html')
-        .pipe($.smoosher())
-        .pipe($.minifyHtml())
-        .pipe(gulp.dest('dist'))
-        .on('end', done);
-    });
+// makes a production build (client => dist)
+gulp.task('default', (done) => {
+  process.env.NODE_ENV = 'production';
+  runSequence(
+    ['copy'],
+    ['scripts', 'styles', 'build-pages'],
+    ['html'/* 'images' */],
+    ['revreplace'],
+  done);
 });
 
-// clears out the dist and .tmp folders
-gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], { dot: true }));
-
-// // runs a development server (serving up .tmp and client)
-gulp.task('serve', ['styles', 'build-pages'], done => {
+// runs a development server (serving up dist and client)
+gulp.task('watch', ['styles', 'build-pages', 'copy'], (done) => {
   const bundlers = getBundlers(true);
 
   // execute all the bundlers once, up front
@@ -189,110 +167,147 @@ gulp.task('serve', ['styles', 'build-pages'], done => {
   initialBundles.on('end', () => {
     // use browsersync to serve up the development app
     browserSync({
-      // notify: false,
+      notify: true,
+      open: process.argv.includes('--open'),
+      ui: process.argv.includes('--bsui'),
+      ghostMode: process.argv.includes('--ghost'),
       port: process.env.PORT || '3000',
       server: {
-        baseDir: ['.tmp', 'client'],
-        routes: {
-          '/bower_components': 'bower_components',
-        },
+        baseDir: 'dist',
       },
     });
 
     // refresh browser after other changes
-    gulp.watch(['client/**/*.html'], ['build-pages', reload]);
-    gulp.watch(['client/styles/**/*.{scss,css}'], ['styles', reload]);
-    gulp.watch(['client/images/**/*'], reload);
+    gulp.watch([
+      'client/**/*.{html,md}',
+      'views/**/*.{js,html}',
+      'config/*.{js,json}'], ['build-pages', reload]);
+    gulp.watch(['client/**/*.scss'], ['styles', reload]);
+    gulp.watch(copyGlob, ['copy', reload]);
+
+    // UNCOMMENT IF USING IMAGEMIN
+    // gulp.watch(['client/images/**/*'], reload);
 
     done();
   });
 });
 
-// builds and serves up the 'dist' directory
-gulp.task('serve:dist', ['build'], done => {
-  require('browser-sync').create().init({
-    open: false,
-    notify: false,
-    server: 'dist',
-  }, done);
+// copies over miscellaneous files (client => dist)
+gulp.task('copy', () =>
+  gulp.src(copyGlob, { dot: true })
+    .pipe(gulp.dest('dist')),
+);
+
+gulp.task('build-pages', () => {
+  delete require.cache[require.resolve('./views')];
+  delete require.cache[require.resolve('./config/flags')];
+  delete require.cache[require.resolve('./config/article')];
+  delete require.cache[require.resolve('./config/index')];
+
+  return gulp.src('client/**/*.html')
+    .pipe(plumber())
+    .pipe(gulpdata(async d => require('./config').default(d)))
+    .pipe(gulpnunjucks.compile(null, { env: require('./views').configure() }))
+    .pipe(gulp.dest('dist'));
 });
+
+// minifies all HTML, CSS and JS (dist & client => dist)
+gulp.task('html', () =>
+  gulp.src('dist/**/*.html')
+    .pipe(inlineSource())
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      processConditionalComments: true,
+    }))
+    .pipe(gulp.dest('dist')),
+);
 
 // task to do a straightforward browserify bundle (build only)
 gulp.task('scripts', () =>
-  mergeStream(getBundlers().map(bundler => bundler.execute()))
+  mergeStream(getBundlers().map(bundler => bundler.execute())),
 );
 
 // builds stylesheets with sass/autoprefixer
-gulp.task('styles', () => gulp.src('client/**/*.scss')
-  .pipe($.sourcemaps.init())
-  .pipe($.sass({ includePaths: 'bower_components' })
-    .on('error', error => {
+gulp.task('styles', () =>
+  gulp.src('client/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      includePaths: 'bower_components',
+      outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : 'expanded',
+    }).on('error', function sassError(error) {
       handleBuildError.call(this, 'Error building Sass', error);
-    })
-  )
-  .pipe($.autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
-  .pipe($.sourcemaps.write('./'))
-  .pipe(gulp.dest('.tmp'))
-);
-
-// lints JS files
-gulp.task('eslint', () => gulp.src('client/scripts/**/*.js')
-  .pipe($.eslint())
-  .pipe($.eslint.format())
-  .pipe($.if(env === 'production', $.eslint.failAfterError()))
+    }))
+    .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
+    .pipe(gulp.dest('dist')),
 );
 
 // renames asset files and adds a rev-manifest.json
 gulp.task('revision', () =>
   gulp.src(['dist/**/*.css', 'dist/**/*.js'])
-    .pipe($.rev())
+    .pipe(rev())
     .pipe(gulp.dest('dist'))
-    .pipe($.rev.manifest())
-    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist')),
 );
 
 // edits html to reflect changes in rev-manifest.json
 gulp.task('revreplace', ['revision'], () =>
   gulp.src('dist/**/*.html')
-    .pipe($.revReplace({ manifest: gulp.src('./dist/rev-manifest.json') }))
-    .pipe(gulp.dest('dist'))
+    .pipe(revReplace({ manifest: gulp.src('./dist/rev-manifest.json') }))
+    .pipe(gulp.dest('dist')),
 );
 
-// sets up watch-and-rebuild for JS and CSS
-gulp.task('watch', done => {
-  runSequence('clean', ['scripts', 'styles', 'build-pages'], () => {
-    gulp.watch('./client/**/*.html', ['build-pages']);
-    gulp.watch('./client/**/*.scss', ['styles']);
-    gulp.watch('./client/**/*.{js,hbs}', ['scripts', 'eslint']);
-    done();
+// IMAGE COMPRESSION:
+// OPTIONAL TASK IF YOU HAVE IMAGES IN YOUR PROJECT REPO
+//  1. install gulp-imagemin:
+//       $ npm i -D gulp-imagemin
+//  2. uncomment task below
+//  3. Find other commented out stuff related to imagemin elsewhere in this gulpfile
+//
+// gulp.task('images', () => gulp.src('dist/**/*.{jpg,png,gif,svg}')
+//   .pipe(gulpimagemin({
+//     progressive: true,
+//     interlaced: true,
+//   }))
+//   .pipe(gulp.dest('dist'))
+// );
+function distServer() {
+  const serveStatic = require('serve-static');
+  const finalhandler = require('finalhandler');
+  const serve = serveStatic('dist', { index: ['index.html'] });
+  return http.createServer((req, res) => {
+    serve(req, res, finalhandler(req, res));
   });
+}
+
+gulp.task('test:install-selenium', (done) => {
+  const selenium = require('selenium-standalone');
+  selenium.install({ version: '2.53.1' }, done);
 });
 
-// makes a production build (client => dist)
-gulp.task('build', done => {
-  env = 'production';
+gulp.task('test:preflight', ['test:install-selenium'], () => {
+  const nightwatch = require('nightwatch');
 
-  runSequence(
-    ['clean', 'eslint'],
-    ['scripts', 'styles', 'copy', 'build-pages'],
-    ['html', 'images'],
-    ['revreplace'],
-  done);
-});
-
-// task to deploy to the interactive server
-gulp.task('deploy', done => {
-  if (!DEPLOY_TARGET) {
-    console.error('Please specify a DEPLOY_TARGET in your gulpfile!');
-    process.exit(1);
+  if (process.env.CIRCLE_PROJECT_REPONAME === 'starter-kit') {
+    console.info('Project is base starter-kit; bypassing preflight checks...');
+    return process.exit();
   }
 
-  igdeploy({
-    src: 'dist',
-    destPrefix: '/var/opt/customer/apps/interactive.ftdata.co.uk/var/www/html',
-    dest: DEPLOY_TARGET,
-  }, error => {
-    if (error) done(error);
-    else console.log(`Deployed to http://ig.ft.com/${DEPLOY_TARGET}/`);
+  if (process.env.CIRCLE_BUILD_NUM === 1) {
+    console.info('Initial build; bypassing preflight checks...');
+    return process.exit();
+  }
+
+  distServer().listen(process.env.PORT || '3000');
+
+  return nightwatch.runner({ // eslint-disable-line consistent-return
+    config: 'nightwatch.json',
+    group: 'preflight',
+  }, (passed) => {
+    if (passed) {
+      process.exit();
+    } else {
+      process.exit(1);
+    }
   });
 });
