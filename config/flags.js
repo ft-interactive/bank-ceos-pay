282e0523f28571660a3816c2c9957d7bb3bdfe58
David Blood <davidbjourno@users.noreply.github.com>
2017-07-23 17:01:48 +0100

    Disable onward journey

--- config/flags.js
+++ config/flags.js
@@ -6,7 +6,7 @@ export default () => ({ // eslint-disable-line
   analytics: prod,
   googleAnalytics: prod,
   ads: false,
-  onwardjourney: true,
+  onwardjourney: false,
   shareButtons: true,
   header: true,
   footer: true,
