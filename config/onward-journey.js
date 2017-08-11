6e1d64cf33446e0e5b16fccc5ae9a2544e048407
Martin Stabe <martinstabe@gmail.com>
2017-08-11 11:07:00 +0100

    Update OJ to v2 tags

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,7 @@
 export default () => ({
   relatedContent: [
-    { rows: 2, list: 'thing/NTg=-U2VjdGlvbnM=' },
+    { rows: 1, list: 'thing/f460326d-d78d-31b4-bb01-c18e2a080945' },
+    { rows: 1, list: 'thing/6951f113-b923-3fd3-9e95-3552b7d01cc6' },
     // { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
   ],
 });
